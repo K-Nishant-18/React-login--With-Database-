5fc1b1676e0fe9dc7df35171aa77edda9a00cb74
KUMAR NISHANT <me.knishant@gmail.com>
2025-03-06 02:52:01 +0530

    Added application.properties for both development and production

--- login-signup-app/spring-login-backend_(MySQL)/demo/src/main/java/com/example/demo/controller/AuthController.java
+++ login-signup-app/spring-login-backend_(MySQL)/demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 
 public class AuthController {
     @Autowired
